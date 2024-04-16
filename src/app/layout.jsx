802283a85524feb9b9ee53f99f07b60c58607145
ikmat-rabib <ikmat.rbb@gmail.com>
2024-04-16 15:18:45 +0600

    fixed navbar text issue

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>vercel 
+      <body className={inter.className}> 
         <Navbar></Navbar>
         <main className="bg-green-300">
         {children}
